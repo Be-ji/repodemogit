a27a6f130ebdc1267ab0f1785ce042cd569a72e5
student <student@PARIS3-B.bt-training.be>
2017-09-04 12:07:46 +0200

    Log 4 j

--- src/main/java/be/bt/MonProjetMavenSousGit/App.java
+++ src/main/java/be/bt/MonProjetMavenSousGit/App.java
@@ -9,5 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        showLoggingFrameworks();
     }
+
+	private static void showLoggingFrameworks() {
+		System.out.println("===Log4===");		
+	}
 }
